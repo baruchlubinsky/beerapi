9dafeda51ceb3c19fcc96076e19ab1515a49c548
Baruch Lubinsky <baruchlubinsky@gmail.com>
2014-06-02 21:28:59 +0200

    POST

--- beerapi.go
+++ beerapi.go
@@ -3,47 +3,72 @@ package main
 import (
 	"github.com/crowdmob/goamz/dynamodb"
 	"github.com/crowdmob/goamz/aws"
-	_"net/http"
-	"fmt"
+	"net/http"
+	"log"
 	"time"
 	"io/ioutil"
 	"strings"
+	"encoding/json"
 )
 
 var dbserver dynamodb.Server
 
 var auth aws.Auth
 
+var beerTable dynamodb.Table
+
 var region = aws.Region {
 	Name: "eu-west-1",
 	DynamoDBEndpoint: "http://dynamodb.eu-west-1.amazonaws.com",
 }
 
+type Beer struct {
+	Name string
+	Type string
+}
+
 func init () {
 	file, _ := ioutil.ReadFile("credentials.csv")
 	lines := strings.Split(string(file), "\n")
-	data := stirng.Split(lines[1], ",")
+	data := strings.Split(lines[1], "\t")
 	auth, err := aws.GetAuth(data[1], data[2], "", time.Now())
 	if err != nil {
 		panic("Unable to connect to aws.")
 	}
 	dbserver = dynamodb.Server {auth, region}
-}
 
-func main () {
-	fmt.Println("Connected to database.")
-
-	beerTable := dynamodb.Table {
+	beerTable = dynamodb.Table {
 		&dbserver,
 		"Beer",
 		dynamodb.PrimaryKey{dynamodb.NewStringAttribute("Name", ""),nil},
 	}
+}
 
-	res, err := beerTable.PutItem("Castle", "", []dynamodb.Attribute{*dynamodb.NewStringAttribute("Type","Lager")})
-	if res {
-		fmt.Println("Created item.")
+func create(response http.ResponseWriter, request *http.Request) {
+	if request.Method == "POST" {
+		body, err := ioutil.ReadAll(request.Body)
+		check(err)
+		var data Beer
+		err = json.Unmarshal(body, &data) 
+		check(err)
+		log.Printf("POST /beer %#v", data)
+		_, err = beerTable.PutItem(data.Name, "", []dynamodb.Attribute{*dynamodb.NewStringAttribute("Type",data.Type)})
+		check(err)
+		response.WriteHeader(http.StatusOK)	
+	} else {
+		response.WriteHeader(400)
 	}
+}
+
+func check(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func main () {
+	log.Println("Connected to database.")
+
+	http.HandleFunc("/beer", create)
+	http.ListenAndServe(":8080", nil)
 }
